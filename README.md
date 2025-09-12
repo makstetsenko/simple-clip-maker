f286627c5e04093b64d7d9b45ea74fb4bbb5724f
Maksym Stetsenko <mxm.stecenko@gmail.com>
2025-09-13 00:38:29 +0300

    Simplify usage

--- README.md
+++ README.md
@@ -13,17 +13,17 @@ source venv/bin/activate
 ### basic usage
 
 ```bash
-python main.py -m "input/*.mp3" -v "input/*.mp4,input/*.m4v,input/*.mov"
+python main.py -i "input"
 ```
 
 ### build plot with beats markers (where clips suppose to be changed)
 
 ```bash
-python main.py -m "input/*.mp3" -p
+python main.py -i "input" -p
 ```
 
 ### store sub clips used to build result video
 
 ```bash
-python main.py --store-sub-clips -m "input/*.mp3" -v "input/*.mp4,input/*.m4v,input/*.mov"
+python main.py --store-sub-clips -i "input"
 ```
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import src.plot_builder as plot_builder
 import src.clip_builder as clip_builder
 import glob
 
+movie_formats = ["m4v", "mov", "mp4", "M4V", "MOV", "MP4"]
 
 def get_args():
     parser = argparse.ArgumentParser(description="Script using Stable Diffusion WebUI API.")
 
-    parser.add_argument("--music-file", "-m", type=str, required=True, help='Path to the music file')
-    parser.add_argument("--video-files-path-template", "-v", type=str, required=False, help="Template to video files. For example './videos/*.mp4' or './videos/*.*' or './videos/my-video.mp4'")
+    parser.add_argument("--input-dir-path", "-i", type=str, required=True, help='Path to the directory with music and video files')
     parser.add_argument("--plot", "-p", action="store_true", required=False, help="Build peaks plot to investigate them")
     parser.add_argument("--store-sub-clips", action="store_true", required=False, help="Build peaks plot to investigate them")
 
@@ -17,17 +17,17 @@ def get_args():
 
 def main():
     args = get_args()
-    
-    music_file_path = glob.glob(args.music_file)[0]
+    music_path_template = args.input_dir_path.rstrip("/") + "/*.mp3"
+    music_file_path = glob.glob(music_path_template)[0]
     
     if args.plot:
         plot_builder.build_plot(music_file_path)
         return
 
     clip_builder.build(
-        audio_file_path_template=args.music_file,
+        audio_file_path_template=music_path_template,
         store_timeline_clips=args.store_sub_clips,
-        video_files_path_template=args.video_files_path_template
+        video_files_path_template=",".join([args.input_dir_path.rstrip("/") + "/*." + x for x in movie_formats])
     )
 
 
